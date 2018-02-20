66866d4c76f78186185e575621ca9356f3f3e4ab
Adrien <adrien.cianci@gmail.com>
2018-02-20 10:04:57 +0100

    Cleanup du code, ajout de commentaires, renommage des structures.

--- src/DataBase/sqlCommandParser.c
+++ src/DataBase/sqlCommandParser.c
@@ -3,58 +3,67 @@
 #include <stdlib.h>
 #include "sqlCommandParser.h"
 
-#define maxArraySize (10)
-#define maxStringSize (30)
-#define nbMaxClauses (8)
-#define nbMaxCommand (4)
+SQL_REQUEST getSqlRequest (char *sqlQuerry){
 
-sqlRequest getSqlRequest (char *sqlQuerry){
   //TODO : Initialiser les structures de façon statique.
-  sqlCommand select = (sqlCommand){
+  SQL_COMMAND select = (SQL_COMMAND){
     .commandName="SELECT",
     .nbMandatoryClauses=2,
     .mandatoryClauseNames={"SELECT","FROM"},
     .nbOptionnalClauses=3,
     .optionnalClauseNames={"WHERE","GROUP BY","HAVING"}
   };
-  sqlCommand insert = (sqlCommand){
+
+  SQL_COMMAND insert = (SQL_COMMAND){
     .commandName="INSERT INTO",
     .nbMandatoryClauses=1,
     .mandatoryClauseNames={"INSERT INTO"},
     .nbOptionnalClauses=1,
     .optionnalClauseNames={"VALUES"}
   };
-  sqlCommand update = (sqlCommand){
+
+  SQL_COMMAND update = (SQL_COMMAND){
     .commandName="UPDATE",
     .nbMandatoryClauses=2,
     .mandatoryClauseNames={"UPDATE","SET"},
     .nbOptionnalClauses=1,
     .optionnalClauseNames={"WHERE"}
   };
-  sqlCommand delete = (sqlCommand){
+
+  SQL_COMMAND delete = (SQL_COMMAND){
     .commandName="DELETE FROM",
     .nbMandatoryClauses=1,
     .mandatoryClauseNames={"DELETE FROM"},
     .nbOptionnalClauses=1,
     .optionnalClauseNames={"WHERE"}
   };
+//Initialisation des variables nécéssaires pour la fonction
+
+  //Taille et tableau permettant de gérer la requète.
   int rqSize = 0;
   char splitedRequest[maxArraySize][maxStringSize];
+
+  //Taille et tableau permettant de gérer les arguments de la requète.
   int argsSize = 0;
   char splitedArgs[maxArraySize][maxStringSize];
+
+  //Taille et tableau permettant de gérer les valeurs de la requète.
   int valuesSize = 0;
   char splitedValues[maxArraySize][maxStringSize];
-  sqlCommand commands[nbMaxCommand]={select,insert,update,delete};
-  splitString(sqlQuerry,splitedRequest,&rqSize," ");
-  //printf("string Array :\n");
-  //printStringArray(splitedRequest,rqSize);
-  sqlCommand command = identifyCommand(splitedRequest,commands);
-  //printSqlCommand(&command);
-  if (isValidSqlQuerry(sqlQuerry,splitedRequest,rqSize,&command)==0){
-    sqlRequest request;
-    if (strcmp("SELECT",command.commandName)==0){
+
+  SQL_COMMAND commands[nbMaxCommand]={select,insert,update,delete}; //Création d'un tableau contenant toutes les commandes gérées par le code.
+
+  splitString(sqlQuerry,splitedRequest,&rqSize," "); //Transformation de la requète en tableau et chaine avec le séparateur " "
+
+  SQL_COMMAND command = identifyCommand(splitedRequest,commands); //Identification de la commande SQL
+
+  if (isValidSqlQuerry(sqlQuerry,splitedRequest,rqSize,&command)==0){ //On vérifie que la requète est correctement formater
+    SQL_REQUEST request;
+
+    if (strcmp("SELECT",command.commandName)==0){ //En fonction du type de requète, on renseigne la structure de façon spécifique.
+
       splitString(splitedRequest[1],splitedArgs,&argsSize,",");
-      request = (sqlRequest){
+      request = (SQL_REQUEST){
         .sqlType=SELECT,
         .nameTable=splitedRequest[3],
         .nbArgs=argsSize,
@@ -63,11 +72,13 @@ sqlRequest getSqlRequest (char *sqlQuerry){
         .listValues={""},
         .where=getWhereClause(splitedRequest,rqSize)};
       mapStringArray(request.listArgs,splitedArgs,argsSize);
+
     }else if (strcmp("INSERT INTO",command.commandName)==0){
+
       splitString(splitedRequest[3],splitedValues,&valuesSize,",");
       removeChar(splitedValues[0],'(');
       removeChar(splitedValues[valuesSize-1],')');
-      request = (sqlRequest){
+      request = (SQL_REQUEST){
         .sqlType = INSERT_INTO,
         .nameTable = splitedRequest[1],
         .nbArgs = 0,
@@ -76,9 +87,11 @@ sqlRequest getSqlRequest (char *sqlQuerry){
         .listValues={""},
         .where=getWhereClause(splitedRequest,rqSize)};
         mapStringArray(request.listValues,splitedValues,valuesSize);
+
     }else if (strcmp("UPDATE",command.commandName)==0){
+
       splitString(splitedRequest[3],splitedArgs,&argsSize,"=");
-      request = (sqlRequest){
+      request = (SQL_REQUEST){
         .sqlType = UPDATE,
         .nameTable = splitedRequest[1],
         .nbArgs = argsSize,
@@ -87,8 +100,10 @@ sqlRequest getSqlRequest (char *sqlQuerry){
         .listValues={""},
         .where=getWhereClause(splitedRequest,rqSize)};
         mapStringArray(request.listArgs,splitedArgs,argsSize);
+
     }else if (strcmp("DELETE FROM",command.commandName)==0){
-      request = (sqlRequest){
+
+      request = (SQL_REQUEST){
         .sqlType = DELETE,
         .nameTable =splitedRequest[1],
         .nbArgs = 0,
@@ -102,18 +117,22 @@ sqlRequest getSqlRequest (char *sqlQuerry){
     return request;
   }
 }
-void printSqlCommand(sqlCommand *command){
+void printSqlCommand(SQL_COMMAND *command){
+
   printf("Nom de la commande : %s\n",command->commandName);
   printf("Clauses obligatoires :\n");
   for (size_t i = 0; i < command->nbMandatoryClauses; i++) {
     printf("%s\n",command->mandatoryClauseNames[i]);
   }
+
   printf("Clauses optionnelles:\n");
   for (size_t i = 0; i < command->nbOptionnalClauses; i++) {
     printf("%s\n",command->optionnalClauseNames[i] );
   }
 }
-int isMandatoryClause(char* string,sqlCommand *command){
+
+int isMandatoryClause(char* string,SQL_COMMAND *command){
+
   for (size_t i = 0; i < command->nbMandatoryClauses; i++) {
     if (strcmp(string,command->mandatoryClauseNames[i])==0) {
       return 0;
@@ -122,7 +141,9 @@ int isMandatoryClause(char* string,sqlCommand *command){
     }
   }
 }
-int isOptionnalClause(char* string,sqlCommand *command){
+
+int isOptionnalClause(char* string,SQL_COMMAND *command){
+
   for (size_t i = 0; i < command->nbOptionnalClauses; i++) {
     if (strcmp(string,command->optionnalClauseNames[i])==0) {
       return 0;
@@ -131,12 +152,15 @@ int isOptionnalClause(char* string,sqlCommand *command){
     }
   }
 }
+
 void printStringArray(char stringArray[][maxStringSize], int arraySize){
   for (size_t i = 0; i < arraySize; i++) {
     printf("%s\n",stringArray[i]);
   }
 }
-void printQuerryStruct (sqlRequest *sqlQuerry){
+
+void printQuerryStruct (SQL_REQUEST *sqlQuerry){
+
   printf("Type de Requete : %d\n",sqlQuerry->sqlType);
   printf("Nom des tables:\n");
   printf("%s\n",sqlQuerry->nameTable );
@@ -148,19 +172,24 @@ void printQuerryStruct (sqlRequest *sqlQuerry){
   for (size_t i = 0; i < sqlQuerry->nbArgs; i++) {
     printf("%s\n",sqlQuerry->listArgs[i] );
   }
+
   printf("Liste des valeurs :\n");
   for (size_t i = 0; i < sqlQuerry->nbValues; i++) {
     printf("%s\n",sqlQuerry->listValues[i] );
   }
+
   printf("Structure Where:\n");
   printf("target value :%s\n",sqlQuerry->where.targetValue );
   printf("Operator :%s\n",sqlQuerry->where.operatorField );
   printf("source value:%s\n",sqlQuerry->where.sourceValue );
 }
+
 void splitString(char* sqlQuerry,char stringArray[][maxStringSize],int *arraySize, const char *splitCharacter){
+
   char *token, *cpySqlQuerry;
   int counter = 0; //création de l'index du tableau
   cpySqlQuerry = strdup(sqlQuerry);  //On réserve la mémoire de la string
+
   while (token = strsep(&cpySqlQuerry, splitCharacter)){//Tant qu'il est possible de segmenter la chaine, affectation de la valeur de la string dans un token
     if (strcmp(token,"INSERT")==0 || strcmp(token,"INTO")==0){
       strcpy(stringArray[0],"INSERT INTO");//gestion de l'exception de INSERT INTO
@@ -171,12 +200,14 @@ void splitString(char* sqlQuerry,char stringArray[][maxStringSize],int *arraySiz
     }else{
     strcpy(stringArray[counter],token);// Copie de la valeur du token dans l'index en cours du tableau.
     counter++; //incrément de l'index du tableau
-  }
+    }
   }
   *arraySize = counter;
   free(cpySqlQuerry);
 }
-int isValidSqlQuerry (char *sqlQuerry, char stringArray[][maxStringSize],int arraySize,sqlCommand *command){
+
+int isValidSqlQuerry (char *sqlQuerry, char stringArray[][maxStringSize],int arraySize,SQL_COMMAND *command){
+
   //Le nombre d'éléments du tableau doit toujours être un multiple de 2, sinon la paire clause / argument n'est pas respecté.
   /*if (arraySize % 2 != 0){
     printf("Nombre d'arguments non valide");
@@ -199,43 +230,58 @@ int isValidSqlQuerry (char *sqlQuerry, char stringArray[][maxStringSize],int arr
     }
     return 0;
 }
-sqlCommand identifyCommand (char stringArray[][maxStringSize],sqlCommand commands[nbMaxCommand]){
+SQL_COMMAND identifyCommand (char stringArray[][maxStringSize],SQL_COMMAND commands[nbMaxCommand]){
+
   for (size_t i = 0; i < nbMaxCommand; i++) {
     //On identifie la commande par la correspondance de la première clause qui est caractéristique de la commande
     if (strcmp(stringArray[0],commands[i].commandName)==0) {
       return commands[i];
     }
   }
-    sqlCommand emptySqlCommand = {"INVALIDE",0,{},0,{}};
+    //Si il est impossible d'indentifier la commande, on renvoie une commande vide.
+    SQL_COMMAND emptySqlCommand = {"INVALIDE",0,{},0,{}};
     return emptySqlCommand;
 }
+
 void mapStringArray (char targetArray[][maxStringSize],char sourceArray[][maxStringSize],int size){
+
   for (size_t i = 0; i < size; i++) {
     strcpy(targetArray[i],sourceArray[i]);
   }
 }
-whereClause getWhereClause (char stringArray[][maxStringSize],int arraySize){
-  whereClause where = (whereClause){.targetValue="",.operatorField="",.sourceValue=""};
+
+WHERE_CLAUSE getWhereClause (char stringArray[][maxStringSize],int arraySize){
+
+  WHERE_CLAUSE where = (WHERE_CLAUSE){.targetValue="",.operatorField="",.sourceValue=""};
   for (size_t i = 0; i < arraySize; i++) {
-    if (strcmp(stringArray[i],"WHERE")==0) {
+    if (strcmp(stringArray[i],"WHERE")==0) { //On cherche la clause WHERE dans le tableau
+
       int whereSize = 0;
       char splitedWhere[maxArraySize][maxStringSize];
-      splitString(stringArray[i+1],splitedWhere,&whereSize,"=");
+
+      //TODO : Gérer les autres opérateurs.
+      splitString(stringArray[i+1],splitedWhere,&whereSize,"=");//Découpage de l'argument de la clause WHERE
+
+      //On affecte les aguments découpés dans la struct WHERE_CLAUSE.
       strcpy(where.targetValue,splitedWhere[0]);
       strcpy(where.operatorField,"=");
       strcpy(where.sourceValue,splitedWhere[1]);
+
     }
   }
   return where;
 }
+
 void removeChar (char string[], char removedChar){
+
   int i,j;
-  for (size_t i = 0; string[i] != 0; ++i) {
-    while (string[i]==removedChar) {
-      j=i;
-      while(string[j]!=0){
-        string[j]=string[j+1];
+  for (size_t i = 0; string[i] != 0; ++i) {//On itère sur tous les caractères de la chaine.
+    while (string[i]==removedChar) {// Tant qu'on trouve le caractère à supprimer.
+      j=i;//j devient l'index du caractère à supprimer.
+      while(string[j]!=0){//Tant que le caractère à supprimer n'est pas le caractère de fin de chaine.
+        string[j]=string[j+1];//Le caractère à supprimer est remplacé par le caractère suivant.
         ++j;
+
       }
     }
   }
--- src/DataBase/sqlCommandParser.h
+++ src/DataBase/sqlCommandParser.h
@@ -2,7 +2,7 @@
 #define maxStringSize (30)
 #define nbMaxClauses (8)
 #define nbMaxCommand (4)
-/*! \enum sqlType
+/*! \enum SQL_TYPE
     \ brief Renseigne le type de commande.
 */
 typedef enum{
@@ -10,39 +10,31 @@ typedef enum{
   INSERT_INTO,
   UPDATE,
   DELETE
-}sqlType;
+}SQL_TYPE;
 
-/*! \struct whereClause
+/*! \struct WHERE_CLAUSE
     \brief Structure représentant une clause WHERE.
 */
-
 typedef struct {
   char targetValue[maxStringSize];
   char operatorField[maxStringSize];
   char sourceValue[maxStringSize];
-}whereClause;
-/*! \enum operatorEnum
-    \brief Renseigne le type d'opérateur utilisé
+}WHERE_CLAUSE;
 
-typedef enum {
-
-}operatorEnum;
-*/
-
-/*! \struct sqlRequest
+/*! \struct SQL_REQUEST
     \brief Structure représentant une requete à la base de données.
 */
 typedef struct{
-  sqlType sqlType;
+  SQL_TYPE sqlType;
   char* nameTable;
   int nbArgs;
   int nbValues;
   char listArgs[maxArraySize][maxStringSize];
   char listValues[maxArraySize][maxStringSize];
-  whereClause where;
-}sqlRequest;
+  WHERE_CLAUSE where;
+}SQL_REQUEST;
 
-/*! \struct sqlCommand
+/*! \struct SQL_COMMAND
     \brief Structure représentant une commande SQL.
 */
 typedef struct{
@@ -51,7 +43,8 @@ typedef struct{
   char* mandatoryClauseNames[nbMaxClauses];
   int nbOptionnalClauses;
   char* optionnalClauseNames[nbMaxClauses];
-}sqlCommand;
+}SQL_COMMAND;
+
 /*! \fn mapStringArray (char targetArray[][maxStringSize],char sourceArray[][maxStringSize],int size)
     \brief Cette fonction tranfere index par index les chaines de caractères du tableau sourceArray vers le tableau targetArray
     \param targetArray tableau cible
@@ -59,6 +52,7 @@ typedef struct{
     \return Pas de retour
 */
 void mapStringArray (char targetArray[][maxStringSize],char sourceArray[][maxStringSize],int size);
+
 /*! \fn  splitString(char* sqlQuerry,char stringArray[][maxStringSize],int *arraySize)
     \brief Cette fonction sépare une chaine de characteres par le séparateur " " en tableau de chaine de caractère.
     \param sqlQuerry chaine de characteres à séparer.
@@ -68,12 +62,14 @@ void mapStringArray (char targetArray[][maxStringSize],char sourceArray[][maxStr
     \return Cette fonction ne renvoie rien.
 */
 void splitString(char* sqlQuerry,char stringArray[][maxStringSize],int *arraySize,const char *splitCharacter);
-/*! \fn  printQuerryStruct(sqlRequest *sqlQuerry)
+
+/*! \fn  printQuerryStruct(SQL_REQUEST *sqlQuerry)
     \brief Cette fonction affiche le contenu de la structure sqlRequest passée en paramètre.
     \param *sqlQuerry instance de la structure à afficher.
     \return Cette fonction ne renvoie rien.
 */
-void printQuerryStruct (sqlRequest *sqlQuerry);
+void printQuerryStruct (SQL_REQUEST *sqlQuerry);
+
 /*! \fn  printStringArray(char stringArray[][maxStringSize],int arraySize)
     \brief Cette fonction affiche le contenu du tableau de chaine de char passé en paramètre.
     \param stringArray Tableau à afficher.
@@ -81,54 +77,62 @@ void printQuerryStruct (sqlRequest *sqlQuerry);
     \return Cette fonction ne renvoie rien.
 */
 void printStringArray(char stringArray[][maxStringSize],int arraySize);
-/*! \fn  printSqlCommand(sqlCommand *command);
-    \brief Cette fonction affiche le contenu de la structure sqlCommand passée en paramètre.
-    \param command reference de la structure sqlCommand à afficher.
+
+/*! \fn  printSqlCommand(SQL_COMMAND *command);
+    \brief Cette fonction affiche le contenu de la structure SQL_COMMAND passée en paramètre.
+    \param command reference de la structure SQL_COMMAND à afficher.
     \return Cette fonction ne renvoie rien.
 */
-void printSqlCommand(sqlCommand *command);
-/*! \fn  isMandatoryClause(char* string,sqlCommand *command);
+void printSqlCommand(SQL_COMMAND *command);
+
+/*! \fn  isMandatoryClause(char* string,SQL_COMMAND *command);
     \brief Cette fonction vérifie si la chaine de char passée en paramètre correspond à une des clauses obligatoire de la commande SQL passée en paramètre
     \param string Clause à analyser.
     \param command Reference à la commande SQL.
     \return Cette fonction renvoie un entier de valeur 0 si string est une clause obligatoire de command. Autrement, renvoie 1.
 */
-int isMandatoryClause(char* string,sqlCommand *command);
-/*! \fn isOptionnalClause(char* string,sqlCommand *command)
+int isMandatoryClause(char* string,SQL_COMMAND *command);
+
+/*! \fn isOptionnalClause(char* string,SQL_COMMAND *command)
     \brief Cette fonction vérifie si la chaine de char passée en paramètre correspond à une des clauses Optionnelle de la commande SQL passée en paramètre.
     \param string Clause à analyser.
     \param command Reference à la commande SQL.
     \return Cette fonction renvoie un entier de valeur 0 si string est une clause Optionnelle de command. Autrement, renvoie 1.
 */
-int isOptionnalClause(char* string,sqlCommand *command);
-/*! \fn isValidSqlQuerry (char *sqlQuerry,char stringArray[][maxStringSize],int arraySize, sqlCommand *command)
+int isOptionnalClause(char* string,SQL_COMMAND *command);
+
+/*! \fn isValidSqlQuerry (char *sqlQuerry,char stringArray[][maxStringSize],int arraySize, SQL_COMMAND *command)
     \brief Cette fonction vérifie la validité d'une requète SQL.
     \param sqlQuerry requete SQL qui sera analysée par la fonction.
     \param stringArray tableau de chaine contenant la requete à analyser.
     \param arraySize taille du tableau de chaine.
     \param command Commande SQL correspondant à la requète.
 */
-int isValidSqlQuerry (char *sqlQuerry,char stringArray[][maxStringSize],int arraySize, sqlCommand *command);
-/*! \fn identifyCommand (char stringArray[][maxStringSize], sqlCommand commands[nbMaxCommand])
+int isValidSqlQuerry (char *sqlQuerry,char stringArray[][maxStringSize],int arraySize, SQL_COMMAND *command);
+
+/*! \fn identifyCommand (char stringArray[][maxStringSize], SQL_COMMAND commands[nbMaxCommand])
     \brief Cette fonction identifie la correspondance entre une requete brut et une commande SQL disponible.
     \param stringArray tableau de chaine contenant la requète SQL brut.
     \param commands tableau de commandes SQL contenant toutes les commandes gérées par le parseur.
     \return Retourne la commande SQL correspondant à la requète.
 */
-sqlCommand identifyCommand (char stringArray[][maxStringSize], sqlCommand commands[nbMaxCommand]);
+SQL_COMMAND identifyCommand (char stringArray[][maxStringSize], SQL_COMMAND commands[nbMaxCommand]);
+
 /*! \fn getSqlRequest (char *sqlQuerry)
     \brief Cette fonction va parser la requète en entrée et renvoyer cette requète sous forme strcuturé.
     \param sqlQuerry Requète SQL à Parser.
     \return Renvoie la structure correspondant à la chaine en entrée. Si la chaine d'entrée est invalide, les champs de la structure seront initialisés à 0.
 */
-sqlRequest getSqlRequest (char *sqlQuerry);
+SQL_REQUEST getSqlRequest (char *sqlQuerry);
+
 /*! \fn getWhereClause (char stringArray[][maxStringSize],int arraySize)
     \brief Cette fonction va détecter la clause WHERE et la convertir en structure.
     \param stringArray Tableau de chaines de caractère contenant le WHERE.
     \param arraySize taille du tableau de chaines.
     \return Renvoie la structure correspondant au WHERE. Si il n'y a pas de WHERE dans le tableau, les champs de la structure renvoyé sont vides.
 */
-whereClause getWhereClause (char stringArray[][maxStringSize],int arraySize);
+WHERE_CLAUSE getWhereClause (char stringArray[][maxStringSize],int arraySize);
+
 /*! \fn removeChar (char string[], char removedChar)
     \brief Cette fonction retire le caractère removeChar de la chaine de caractère string
     \param string Chaine de caractère contenant le caractère à supprimer.
