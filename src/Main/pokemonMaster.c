3a27e96b7866ec0632dad5ba98f11954fce0fc78
gartux <toto>
2018-02-22 17:40:18 +0100

    Nettoyage pokemonMaster.c

--- src/Main/pokemonMaster.c
+++ src/Main/pokemonMaster.c
@@ -16,33 +16,6 @@ int main(int argc, char const *argv[]) {
     return 1;
   } else {
     menu(dataBase);
-  /*restoreTables(dataBase);
-
-  query = excuteQuery(dataBase, "SELECT * FROM Pokemon WHERE id=1");
-
-  if (query != NULL) {
-    for(int i = 0; i < query->nbRecord; i++) {
-      char * field;
-      for(int j = 0; j < query->descriptor.nbField; j++) {
-        field = getDataQueryById(query, i, j);
-        switch (getTypeQueryById(query, j)) {
-          case DATA_FIELD_PK:
-          case DATA_FIELD_INT:
-          {
-            DEBUG("Champ %d %s : %d", j+1, getNameQueryById(query, j), (int)*field);
-          }
-          break;
-          case DATA_FIELD_CHAR:
-          {
-            DEBUG("Champ %d %s : %s",j+1, getNameQueryById(query, j), field);
-          }
-          break;
-          default:
-          break;
-        }
-      }
-    }*/
-
     closeQuery(query);
   }
 
